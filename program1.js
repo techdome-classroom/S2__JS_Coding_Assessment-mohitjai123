f0eb5eeb348e0a9a436a7ee54fb9a3e7ac353649
mohit123 <jaiswalmohit217@gmail.com>
2024-09-25 14:37:32 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,14 +3,9 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    var countSL = 0;
-    var countSR = 0;
-    var countML = 0;
-    var countMR = 0;
-    var countLL = 0;
-    var countLR = 0;
+    let counts = {}
     for(i=0; i<s.length; i++){
-      
+        
     }
 
 };
