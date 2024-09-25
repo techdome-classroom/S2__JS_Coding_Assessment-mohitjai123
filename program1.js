705b404cb6f0b8df5fccba4d32eef43df11ca6f0
mohit123 <jaiswalmohit217@gmail.com>
2024-09-25 14:50:56 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -26,7 +26,7 @@ var isValid = function(s) {
           case '[':
             countLL++;
             break;
-          default:
+          case ']':
             countLR++;
             break;
         }
