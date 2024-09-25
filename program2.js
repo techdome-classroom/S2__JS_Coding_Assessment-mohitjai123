ab85d16e0d17813fc2edcc8742bb57bfac700ce9
mohit123 <jaiswalmohit217@gmail.com>
2024-09-25 15:02:44 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,28 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+  for(i=0; i<s.length; i++){
+    switch(s[i]){
+      case '(':
+        countSL++;
+        break;
+      case ')':
+        countSR++;
+        break;
+      case '{':
+        countML++;
+        break;
+      case "}":
+        countMR++;
+        break;
+      case '[':
+        countLL++;
+        break;
+      default:
+        countLR++;
+        break;
+    }
+}
 };
 
 
